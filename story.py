6f04050080871dfc2f4b0b3a419904de5566c383
elpit08 <pgdr250920@gmail.com>
2026-08-13 15:40:00 +0000

    made a new string and used upper

--- story.py
+++ story.py
@@ -1,13 +1,15 @@
 def main():
-    name = input("what is your name?")
-    color = input("Tell me a color:")
-    adjetive = input("Tell me an adjetive:")
+    name = input("what is your name?").strip().title()
+    color = input("Tell me a color:").strip()
+    adjetive = input("Tell me an adjetive:").strip()
     goal = input("What is a goal you have:")
 
     print(f"Hello, {name}!", end="\n\n")
     print("This is your story:")
-    print(f" When I was in the school I {adjetive} and didnt think to much about it")
-    print(f" but when I looked at my {color} friend I remembered I wanted to {goal}")
+
+    print(f" When I was in the school I {adjetive} and didnt think to much about it \n",f"but when I looked at my {color} friend I remembered I wanted to {goal}")
+    print(f" When I was in the school I {adjetive} and didnt think to much about it \n",f"but when I looked at my {color} friend I remembered I wanted to {goal}".upper())
+
 
 
     #planet = input("Planet:")
