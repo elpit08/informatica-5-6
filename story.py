d8136fe0fd2dc9acd1b86ee269cdb535d71a4762
elpit08 <pgdr250920@gmail.com>
2026-08-13 15:03:32 +0000

    learned the input and print for pyton

---
+++ story.py
@@ -0,0 +1,25 @@
+def main():
+    name = input("what is your name?")
+    color = input("Tell me a color:")
+    adjetive = input("Tell me an adjetive:")
+    goal = input("What is a goal you have:")
+
+    print(f"Hello, {name}!")
+    print("This is your story:")
+    print(f" When I was in the school I {adjetive} and didnt think to much about it")
+    print(f" but when I looked at my {color} friend I remembered I wanted to {goal}")
+
+
+    #planet = input("Planet:")
+    #Separation
+    #print("Hello", planet)
+    #concatenation
+    #print("Hello " +planet)
+    #Formatted Strings
+    #print(f"Hello {planet}")
+    #Ending
+    #print("Hello", end=" ")
+    #print(planet)
+
+if __name__ == "__main__":
+    main()
