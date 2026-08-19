0f71cd2a84d4d36ce1073028867db2ac617a8d67
elpit08 <pgdr250920@gmail.com>
2026-08-19 18:00:05 +0000

    the modular operators examples

---
+++ update-varable.py
@@ -0,0 +1,31 @@
+def main():
+#Augmented Assignment Operator
+    score = 10
+    score += 10
+    print(score)
+
+#Substraction Assignment Operator
+    #x= x-y
+    #x-=y
+    scor = 100
+    scor -= 15
+    print(scor)
+
+#Multiplication Assignment Operator
+    sco = 50
+    sco *= 2
+    print(sco)
+
+#Division Assignment Operator
+    sc = 100
+    sc /=5
+    print(int(sc))
+
+#Modular Assignment Operator
+    s = 10
+    s %= 4
+    print(s)
+
+
+if __name__=="__main__":
+    main()
