cb497ba69ec469ef860fbd78bc067cbd53b2a5af
elpit08 <pgdr250920@gmail.com>
2026-08-24 15:01:04 +0000

    password and have to complete the challenge

---
+++ password.py
@@ -0,0 +1,18 @@
+def main():
+    password= "dejamejulian"
+    try_password= input("What is the password: \n")
+
+    if try_password == password:
+        print("Login was succesful")
+
+    print("Autodestruct the program")
+
+
+if __name__=="__main__":
+    main()
+
+check_password(
+    input("Guess the password: ")
+    
+)
+
