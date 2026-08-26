7899a20a81b271a393fc1231ec398343f5b3c51b
elpit08 <pgdr250920@gmail.com>
2026-08-26 18:11:07 +0000

    todavia no acabo la calculadora

---
+++ if_exercises.py
@@ -0,0 +1,22 @@
+def main():
+    eq = input("Input equation: ")
+    equation = eq.split( )
+    number1 = float(equation[0])
+    operation = equation[1]
+    number2 = float(equation[2])
+
+    if operation == +:
+        print(number1 + number2)
+
+    elif operation == -:
+        print(number1 - number2)
+
+    elif operation == *:
+        print(number1 * number2)
+
+    elif operation == /:
+        print(number1 / number2)
+
+
+if __name__=="__main__":
+    main()
