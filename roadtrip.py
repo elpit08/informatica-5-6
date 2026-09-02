2c6dc287e690e2ba3ceec8b94533f491dca2fbd4
elpit08 <pgdr250920@gmail.com>
2026-09-02 17:54:31 +0000

    made a simple loop about the shrek movie

---
+++ roadtrip.py
@@ -0,0 +1,13 @@
+def main():
+
+#is it a finite group? yes
+    response = ""
+
+    while response != 'yes!':
+        response = input("Are we there yet? ").lower().strip()
+        if response == 'yes':
+            input("really? ")
+    print("We are finally here")
+
+if __name__=="__main__":
+    main()
