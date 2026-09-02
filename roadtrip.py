feb60afeecd5d0c8f4a756a33c34e179fbf606d9
elpit08 <pgdr250920@gmail.com>
2026-09-02 18:01:54 +0000

    had to add the really part of the scene

--- roadtrip.py
+++ roadtrip.py
@@ -2,11 +2,17 @@ def main():
 
 #is it a finite group? yes
     response = ""
+    followup = ""
 
     while response != 'yes!':
         response = input("Are we there yet? ").lower().strip()
+
         if response == 'yes':
-            input("really? ")
+            followup = input("really? ").lower().strip()
+
+        if followup == 'yes':
+            break
+
     print("We are finally here")
 
 if __name__=="__main__":
