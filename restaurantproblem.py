fcfb167df8f82c48d4089b0fb1afa5f5062b4112
elpit08 <pgdr250920@gmail.com>
2026-09-07 14:58:47 +0000

    mis problemas personales 1

---
+++ restaurantproblem.py
@@ -0,0 +1,17 @@
+import random
+
+def main():
+
+    pizza = ["little ceasar","House","Fogueria","La sierra","Venezia"]
+    asiatica = ["sushi box","Pon pon","Wok chicken","Dragon express","kung fu"]
+    mexicana = ["Sirloncito","tacos rosas","Piporros","Carnitas michoacanas","Bronco tacos",]
+    hambrbr = ["Wendys","Rodeo","Erick burguer"]
+    mariscos = ["Las Palmas","Miramar"]
+    pollo = ["Pollo feliz","KFC","Chicken country","fire food","Habanero wings","Pollo rostizado"]
+    variedad = ["hot dog","Bandidos","Munchfries","Subway","Pompeii"]
+
+
+
+
+if __name__=="__main__":
+    main()
