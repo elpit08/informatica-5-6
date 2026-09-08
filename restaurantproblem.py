7d492f34871f85787b25322e2ae773572b5e17cf
elpit08 <pgdr250920@gmail.com>
2026-09-08 20:21:03 +0000

    almost finish my coding final project term 1

--- restaurantproblem.py
+++ restaurantproblem.py
@@ -2,15 +2,51 @@ import random
 
 def main():
 
-    pizza = ["little ceasar","House","Fogueria","La sierra","Venezia"]
-    asiatica = ["sushi box","Pon pon","Wok chicken","Dragon express","kung fu"]
-    mexicana = ["Sirloncito","tacos rosas","Piporros","Carnitas michoacanas","Bronco tacos",]
-    hambrbr = ["Wendys","Rodeo","Erick burguer"]
-    mariscos = ["Las Palmas","Miramar"]
-    pollo = ["Pollo feliz","KFC","Chicken country","fire food","Habanero wings","Pollo rostizado"]
-    variedad = ["hot dog","Bandidos","Munchfries","Subway","Pompeii"]
+    restaurants= ["little ceasar","House","Fogueria","La sierra","Venezia","sushi box","Pon pon","Wok chicken","Dragon express","kung fu","Sirloncito","tacos rosas","Piporros","Carnitas michoacanas","Bronco tacos","Wendys","Rodeo","Erick burguer","Las Palmas","Miramar","Pollo feliz","KFC","Chicken country","fire food","Habanero wings","Pollo rostizado","hot dog","Bandidos","Munchfries","Subway","Pompeii"]
 
+    #5 pizza, 5 asians, 5 mexican, 3 hamburguer, 2 mariscos, 6 pollo, 5 otros
+    option = input("Choice a mode: Random or delimited: ").strip().lower()
+    anwser = ""
+    if option == 'random':
+        print("I will chose a random restaurant")
+        print("Write 'ok' to select the restaurant")
+        while anwser != 'ok':
+            print(random.choice(restaurants))
+            anwser = input("Is the choice okey? ").lower().strip()
 
+    elif option == 'delimited':
+        print("Say what restaurant, you dont want to go to")
+        print("Write 'ok' to select the restaurant")
+
+        print("Select a type: 1)pizza     2)asian \n 3)mexican       4)hamburger\n 5)mariscos     6)pollo\n 7)otro")
+        restaurant_type = input("Selection: ").lower().strip()
+        while anwser != 'ok':
+            if restaurant_type == 'pizza':
+                pizza = random.randint(0,4)
+                print(restaurants[pizza])
+                anwser = input("Is the choice okey? ")
+
+            elif restaurant_type == 'asian':
+                asian = random.randint(5,9)
+                print(restaurants[asian])
+                anwser = input("Is the choice okey? ")
+
+            elif restaurant_type == 'mexican':
+
+            elif restaurant_type == 'hamburger':
+
+            elif restaurant_type == 'mariscos':
+
+            elif restaurant_type == 'pollo':
+
+            elif restaurant_type == 'otro':
+
+            else:
+                print("Selection not valid")
+                break
+
+    else:
+        print("Select a valid mode")
 
 
 if __name__=="__main__":
