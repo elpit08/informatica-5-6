6df69f5cad9933de7bdd507cc4738cc455589def
elpit08 <pgdr250920@gmail.com>
2026-09-18 21:56:14 +0000

    the list-reference wasnt finished so I had to finished

--- for-intro.py
+++ for-intro.py
@@ -1,10 +1,10 @@
 def main():
 
     doctrine = ["Faith in jesus christ","Repentance","Baptism by water","Confirmation","Endure to the end"]
-    list_i = 1
-    for _ in doctrine:
-        print(f"{list_i}. {_}")
-        list_i += 1
+
+    for _ in range(len(doctrine)):
+        print(f"{_ + 1}. {doctrine[_]}")
+
 
 
 
---
+++ list-reference.py
@@ -0,0 +1,53 @@
+def main():
+    x = len("hello")
+
+
+    print(x)
+
+
+    password = [1, 2, 3, 4, 5, 6, 7, 8]
+
+    xx = ["a", "b", "c", "d"]
+
+    xx.append("e")
+
+
+    print(len(xx))
+
+    print(password)
+
+
+
+
+    change = input(" ")
+
+    if change == "y":
+    password[1]= 4
+    password[0] = 2
+    password[7] = 1
+    password[6] = 9
+    password[5] = 10
+    password.insert(0,99)
+    password.insert(2,42)
+    print(password)
+
+
+    tacos = ["Ojo","tripa","asada","pastor","mixtos"]
+    print(tacos.pop(2))
+    print(tacos)
+    print(tacos.remove("Ojo"))
+    print(tacos)
+
+    tacos.append("gobernador")
+    print(tacos)
+
+    print(max(xx))
+    print(min(xx))
+    print(sum(xx))
+    xx.sort()
+    print(xx)
+
+
+
+if __name__=="__main__"
+    main()
