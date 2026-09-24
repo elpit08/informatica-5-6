781179595264e660c75cd4b9707a1bdee34e0fed
elpit08 <pgdr250920@gmail.com>
2026-09-24 14:44:42 +0000

    finally finished the tables . py and made it work, the order does really matter

--- tables.py
+++ tables.py
@@ -1,12 +1,14 @@
 def main():
-    table = int(input("Enter a number (1-10) "))
-    exit = str(table).lower().strip()
-    while exit != 'exit':
+    table = input("Enter a number (1-10) ")
+    #number = int(table)
+    while table != 'exit':
+        number = int(table)
         for _ in range(1,11):
-            print(f"{_} times {table} is {_ * table}")
 
-        table = int(input("Enter a number (1-10) "))
-        exit = str(table).lower().strip()
+            print(f"{_} times {number} is {_ * number}")
+
+        table = input("Enter a number (1-10) ")
+
 
 if __name__=="__main__":
     main()
