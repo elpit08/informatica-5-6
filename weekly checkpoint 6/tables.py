96e856f546372c79410133e5186894ae2e2f71c8
elpit08 <pgdr250920@gmail.com>
2026-09-24 15:23:20 +0000

    weekly checkpoint 6

--- weekly checkpoint 6/tables.py
+++ weekly checkpoint 6/tables.py
@@ -1,13 +1,15 @@
 def main():
     table = input("Enter a number (1-10) ")
-    #number = int(table)
     while table != 'exit':
         number = int(table)
-        for _ in range(1,11):
+        max_value = int(input("Enter a maximum value of the times table: "))
+        for _ in range(1,max_value + 1):
 
             print(f"{_} times {number} is {_ * number}")
 
-        table = input("Enter a number (1-10) ")
+
+        table = input("Enter a number (1-10) ").lower().strip()
+
 
 
 if __name__=="__main__":
